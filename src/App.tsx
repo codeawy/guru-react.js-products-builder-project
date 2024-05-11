496f001fb7a17f6a7709f434d4a5b2473b4f7631
codeawy <codeawy@gmail.com>
2024-05-11 22:23:53 +0300

    add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import { fakeProductList } from "./lib/fakeData";
 
 function App() {
@@ -8,11 +9,27 @@ function App() {
         {fakeProductList.map((product) => (
           <div
             key={product.id}
-            className="cursor-pointer rounded-lg border p-3 duration-500 hover:bg-purple-500 hover:text-white"
+            className="space-y-3 rounded-lg border p-3 duration-500"
           >
+            <img src="https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+
             <h4>{product.title}</h4>
             <p>{product.description}</p>
-            <span>{product.price}</span>
+            <div>
+              <span>🟢</span>
+              <span>🟢</span>
+              <span>🟢</span>
+            </div>
+            <div className="flex items-center justify-between gap-3">
+              <span>500,000</span>
+              <span>Cars</span>
+            </div>
+            <div className="flex items-center justify-between gap-3">
+              <Button className="flex-1">Edit</Button>
+              <Button className="flex-1" variant={"destructive"}>
+                Destroy
+              </Button>
+            </div>
           </div>
         ))}
       </div>
