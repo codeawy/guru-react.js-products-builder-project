d84d7f2314a4e223231c939d60821ef5d30f9f31
codeawy <codeawy@gmail.com>
2024-05-11 22:13:37 +0300

    Generate a fake data, and display products cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { fakeProductList } from "./lib/fakeData";
 
 function App() {
+  // ** Responsive => Mobile first
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="container">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {fakeProductList.map((product) => (
+          <div
+            key={product.id}
+            className="cursor-pointer rounded-lg border p-3 duration-500 hover:bg-purple-500 hover:text-white"
+          >
+            <h4>{product.title}</h4>
+            <p>{product.description}</p>
+            <span>{product.price}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
